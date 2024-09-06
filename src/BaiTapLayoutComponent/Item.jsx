29e098302ba73c12cfeacef1aa1ea4f85061662d
congvo97 <conghtc97@gmail.com>
2024-09-06 23:49:53 +0700

    bt react buoi1

---
+++ src/BaiTapLayoutComponent/Item.jsx
@@ -0,0 +1,152 @@
+import React, { Component } from "react";
+
+export default class Item extends Component {
+  render() {
+    return (
+      <div className="pt-4 Item">
+        <div className="container px-lg-5">
+          <div className="row gx-lg-5">
+            <div className="col-lg-6 col-xxl-4 mb-5">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                  <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={16}
+                      height={16}
+                      fill="currentColor"
+                      className="bi bi-collection-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6zM2 3a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11A.5.5 0 0 0 2 3m2-2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7A.5.5 0 0 0 4 1" />
+                    </svg>
+                  </div>
+                  <h2 className="fs-4 fw-bold">Fresh new layout</h2>
+                  <p className="mb-0">
+                    With Bootstrap 5, we've created a fresh new layout for this
+                    template!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-xxl-4 mb-5">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                  <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={16}
+                      height={16}
+                      fill="currentColor"
+                      className="bi bi-collection-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6zM2 3a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11A.5.5 0 0 0 2 3m2-2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7A.5.5 0 0 0 4 1" />
+                    </svg>
+                  </div>
+                  <h2 className="fs-4 fw-bold">Free to download</h2>
+                  <p className="mb-0">
+                    As always, Start Bootstrap has a powerful collectin of free
+                    templates.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-xxl-4 mb-5">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                  <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={16}
+                      height={16}
+                      fill="currentColor"
+                      className="bi bi-collection-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6zM2 3a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11A.5.5 0 0 0 2 3m2-2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7A.5.5 0 0 0 4 1" />
+                    </svg>
+                  </div>
+                  <h2 className="fs-4 fw-bold">Jumbotron hero header</h2>
+                  <p className="mb-0">
+                    The heroic part of this template is the jumbotron hero
+                    header!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-xxl-4 mb-5">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                  <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={16}
+                      height={16}
+                      fill="currentColor"
+                      className="bi bi-collection-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6zM2 3a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11A.5.5 0 0 0 2 3m2-2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7A.5.5 0 0 0 4 1" />
+                    </svg>
+                  </div>
+                  <h2 className="fs-4 fw-bold">Feature boxes</h2>
+                  <p className="mb-0">
+                    We've created some custom feature boxes using Bootstrap
+                    icons!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-xxl-4 mb-5">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                  <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={16}
+                      height={16}
+                      fill="currentColor"
+                      className="bi bi-collection-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6zM2 3a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11A.5.5 0 0 0 2 3m2-2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7A.5.5 0 0 0 4 1" />
+                    </svg>
+                  </div>
+                  <h2 className="fs-4 fw-bold">Simple clean code</h2>
+                  <p className="mb-0">
+                    We keep our dependencies up to date and squash bugs as they
+                    come!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-xxl-4 mb-5">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                  <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={16}
+                      height={16}
+                      fill="currentColor"
+                      className="bi bi-collection-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6zM2 3a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11A.5.5 0 0 0 2 3m2-2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7A.5.5 0 0 0 4 1" />
+                    </svg>
+                  </div>
+                  <h2 className="fs-4 fw-bold">A name you trust</h2>
+                  <p className="mb-0">
+                    Start Bootstrap has been the leader in free Bootstrap
+                    templates since 2013!
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
